478e7b2a5ef3e8b09d89e139c377848faacee63f
dy-tech <dayeon964@naver.com>
2020-12-21 23:49:52 +0900

    Lombok code

--- src/main/java/com/ldayeon/springboot/web/HelloController.java
+++ src/main/java/com/ldayeon/springboot/web/HelloController.java
@@ -1,4 +1,4 @@
-package com.ldayeon.springboot;
+package com.ldayeon.springboot.web;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
